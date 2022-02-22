9e33006090f7dcdbc4658b1c95c4b8bb5d6187b3
Larissa Ferreira <larissadantasaf@gmail.com>
2022-02-22 12:45:31 +0100

    exercise-lab-express-pokemon-api

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ const express = require("express");
 
 const PORT = 4000;
 
+const app = express();
+
+app.use(express.json());
+
 // Importing all the pokemon for our data file
 const allPokemon = require("./data");
 
-const app = express();
+app.get("/pokemon", (req, res) => {
+    return res.status(200).json(allPokemon);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+    const {id} = req.params;
+    const findPokemonId = allPokemon.find(
+        (currentElement) => currentElement.id.toString() === id);
+
+    return res.status(200).json(findPokemonId);
+
+});
+
+// app.get("/pokemon/search", (req, res) => {
+//     const {name} = req.query; 
+//     const searchPokemon = allPokemon.filter(
+//         (currentElement) => currentElement.name.toLowerCase().includes(name.toLowerCase())
+//     );
+
+//     return res.status(200).json(searchPokemon);
+// })
 
 // -- Define your route listeners here! --
 
